f57767f82ac2aa570c38e71ae1d59641cfb59e60
Joe Connor <joe.connor@livestation.com>
2013-02-23 11:20:17 +0000

    add spec

---
+++ spec/twitter_word_source_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe LorumIpsumWordSource do
+
+  let :source do
+    TwitterWordSource.new
+  end
+  
+  it 'should load a feed from Twitter' do
+    pending
+  end
+  
+end
